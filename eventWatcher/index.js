5d81d6bea8f667a02f488637d41fe19d2d4e19fb
ssskram <ssskram@ssskram.local>
2019-03-18 10:22:37 -0400

    alertInternationalLogins

--- eventWatcher/index.js
+++ eventWatcher/index.js
@@ -1,5 +1,7 @@
 
 const fetch = require('node-fetch')
+const moment = require('moment')
+const tz = require('moment-timezone')
 global.Headers = fetch.Headers
 
 module.exports = async (context, req) => {
@@ -14,22 +16,40 @@ module.exports = async (context, req) => {
 
     const events = await response.json()
     for (const event in events) {
-        console.log(event)
-        await saveToTable(event)
+        await saveToTable(events[event])
+        if (events[event].country != "US") {
+            await alertInternationalLogins(events[event])
+        }
+    }
+
+    async function saveToTable(event) {
+        await fetch("https://az-table.azurewebsites.us/activeDirectory/newEvent", {
+            method: 'post',
+            headers: new Headers({
+                'Authorization': 'Bearer ' + process.env.TABLE,
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify(event)
+        })
+        .then(res => context.log(res.status))
+        return
+    }
+
+    function alertInternationalLogins(event) {
+        fetch("https://baloo.azurewebsites.us/activeDirectory/alert", {
+            method: 'POST',
+            headers: new Headers({
+                'Authorization': 'Bearer ' + process.env.BALOO,
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify({
+                user: event.userName,
+                country: event.country,
+                time: moment(event.time).tz('America/New_York').format('MM-DD-YYYY, hh:mm A'),
+                id: event.id
+            })
+        })
     }
 
     context.done()
-}
-
-const saveToTable = async (event) => {
-    
-    await fetch("https://az-table.azurewebsites.us/activeDirectory/newEvent", {
-        method: 'post',
-        headers: new Headers({
-            'Authorization': 'Bearer ' + process.env.TABLE,
-            'Content-Type': 'application/json'
-        }),
-        body: JSON.stringify(event)
-    })
-    return
 }
